7cec41ee497bbe6f6846d1323c7b20772b0cc162
Yuya Kato <yuyakato@gmail.com>
2009-10-08 12:34:23 +0900

    リファクタリング

--- lib/wsse/username_token_parser.rb
+++ lib/wsse/username_token_parser.rb
@@ -3,22 +3,10 @@
 module Wsse
   module UsernameTokenParser
     def self.parse(value)
-      pattern1 = /\AUsernameToken Username="(.+?)", PasswordDigest="(.+?)", Nonce="(.+?)", Created="(.+?)"\z/
-      pattern2 = /\AUsernameToken Username="(.+?)", PasswordDigest="(.+?)", Created="(.+?)", Nonce="(.+?)"\z/
-
-      if pattern1 =~ value.to_s
-        return {
-          "Username"       => $1,
-          "PasswordDigest" => $2,
-          "Nonce"          => $3,
-          "Created"        => $4,
-        }
-      elsif pattern2 =~ value.to_s
-        return {
-          "Username"       => $1,
-          "PasswordDigest" => $2,
-          "Created"        => $3,
-          "Nonce"          => $4,
+      if /\AUsernameToken (.+?=".+?"(?:, .+?=".+?")*)\z/ =~ value.to_s
+        return $1.scan(/(?:\A|, )(.+?)="(.+?)"/).inject({}) { |memo, (key, value)|
+          memo[key] = value
+          memo
         }
       else
         return nil
