1bb046d08057cc6360ec7fec3139b25d18c63987
Yuya Kato <yuyakato@gmail.com>
2009-10-08 10:58:08 +0900

    ユニットテストの雛形を作成

---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+require "test/unit"
+require "rubygems"
+
+begin
+  require "redgreen"
+rescue LoadError
+  # nop
+end
+
+begin
+  require "win32console" if /win32/ =~ RUBY_PLATFORM
+rescue LoadError
+  # nop
+end
+
+$:.unshift(File.dirname(__FILE__) + "/../lib")
---
+++ test/wsse_username_token_parser.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require File.dirname(__FILE__) + "/test_helper"
+
+class WsseUsernameTokenParserTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_true
+    assert true
+  end
+end
