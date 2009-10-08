f7438c1f622e6d72892a9871077d8762c42a2aab
Yuya Kato <yuyakato@gmail.com>
2009-10-08 11:37:03 +0900

    簡単な解析処理を追加

--- lib/wsse/username_token_parser.rb
+++ lib/wsse/username_token_parser.rb
@@ -1,1 +1,20 @@
 # coding: utf-8
+
+module Wsse
+  module UsernameTokenParser
+    def self.parse(value)
+      pattern = /\AUsernameToken Username="(.+?)", PasswordDigest="(.+?)", Nonce="(.+?)", Created="(.+?)"\z/
+
+      if pattern =~ value.to_s
+        return {
+          "Username"       => $1,
+          "PasswordDigest" => $2,
+          "Nonce"          => $3,
+          "Created"        => $4,
+        }
+      else
+        return nil
+      end
+    end
+  end
+end
--- test/wsse_username_token_parser_test.rb
+++ test/wsse_username_token_parser_test.rb
@@ -1,12 +1,43 @@
 # coding: utf-8
 
 require File.dirname(__FILE__) + "/test_helper"
+require "wsse/username_token_parser"
 
 class WsseUsernameTokenParserTest < Test::Unit::TestCase
   def setup
+    @mod = Wsse::UsernameTokenParser
   end
 
-  def test_true
-    assert true
+  def test_parse__1
+    # http://www.teria.com/~koseki/tools/wssegen/
+    # UserName: test, Password: test, Nonce: auto, Created: auto
+    value = %|UsernameToken Username="test", PasswordDigest="XYuMkeTAdwEwKhyU/4uw/pbvqrc=", Nonce="Mzg5ODU5MDExYTljODQ5Yg==", Created="2009-10-08T02:31:57Z"|
+    expected = {
+      "Username"       => "test",
+      "PasswordDigest" => "XYuMkeTAdwEwKhyU/4uw/pbvqrc=",
+      "Nonce"          => "Mzg5ODU5MDExYTljODQ5Yg==",
+      "Created"        => "2009-10-08T02:31:57Z", # FIXME: str -> time
+    }
+    assert_equal(expected, @mod.parse(value))
+  end
+
+  def test_parse__2
+    # http://d.hatena.ne.jp/keyword/%A4%CF%A4%C6%A4%CA%A5%D5%A5%A9%A5%C8%A5%E9%A5%A4%A5%D5AtomAPI
+    value = %|UsernameToken Username="hatena", PasswordDigest="ZCNaK2jrXr4+zsCaYK/YLUxImZU=", Nonce="Uh95NQlviNpJQR1MmML+zq6pFxE=", Created="2005-01-18T03:20:15Z"|
+    expected = {
+      "Username"       => "hatena",
+      "PasswordDigest" => "ZCNaK2jrXr4+zsCaYK/YLUxImZU=",
+      "Nonce"          => "Uh95NQlviNpJQR1MmML+zq6pFxE=",
+      "Created"        => "2005-01-18T03:20:15Z", # FIXME: str -> time
+    }
+    assert_equal(expected, @mod.parse(value))
+  end
+
+  def test_parse__nil
+    assert_equal(nil, @mod.parse(nil))
+  end
+
+  def test_parse__empty
+    assert_equal(nil, @mod.parse(""))
   end
 end
