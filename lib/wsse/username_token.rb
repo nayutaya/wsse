32dddea3a9c895138bb6ca0f7ea75cb253136bfb
Yuya Kato <yuyakato@gmail.com>
2009-10-28 18:33:02 +0900

    初期化処理とアクセサを追加

--- lib/wsse/username_token.rb
+++ lib/wsse/username_token.rb
@@ -1,4 +1,14 @@
 # coding: utf-8
 
 module Wsse
+  class UsernameToken
+    def initialize(username, digest, nonce, created)
+      @username = username
+      @digest   = digest
+      @nonce    = nonce
+      @created  = created
+    end
+
+    attr_reader :username, :digest, :nonce, :created
+  end
 end
--- test/wsse_username_token_test.rb
+++ test/wsse_username_token_test.rb
@@ -5,9 +5,14 @@ require "wsse/username_token"
 
 class WsseUsernameTokenTest < Test::Unit::TestCase
   def setup
+    @klass = Wsse::UsernameToken
   end
 
-  def test_ok
-    assert(true)
+  def test_initialize_and_accessor
+    token = Wsse::UsernameToken.new("username", "digest", "nonce", "created")
+    assert_equal("username", token.username)
+    assert_equal("digest",   token.digest)
+    assert_equal("nonce",    token.nonce)
+    assert_equal("created",  token.created)
   end
 end
