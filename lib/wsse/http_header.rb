eacfc489c8be5b9fffba5fc8a707c3924a7a76e2
Yuya Kato <yuyakato@gmail.com>
2009-10-28 13:43:59 +0900

    トークンを生成するメソッドを追加

--- lib/wsse/http_header.rb
+++ lib/wsse/http_header.rb
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+require "wsse/username_token_builder"
+
 module Wsse
   class HttpHeader
     def initialize(username, password)
@@ -8,5 +10,9 @@ module Wsse
     end
 
     attr_reader :username, :password
+
+    def create_token(nonce = nil, created = nil)
+      return UsernameTokenBuilder.create_token(@username, @password, nonce, created)
+    end
   end
 end
--- test/wsse_http_header_test.rb
+++ test/wsse_http_header_test.rb
@@ -2,6 +2,7 @@
 
 require File.dirname(__FILE__) + "/test_helper"
 require "wsse/http_header"
+require "wsse/username_token_parser"
 
 class WsseHttpHeaderTest < Test::Unit::TestCase
   def setup
@@ -18,5 +19,30 @@ class WsseHttpHeaderTest < Test::Unit::TestCase
     header = @klass.new("a", "b")
     assert_equal("a", header.username)
     assert_equal("b", header.password)
+  end
+
+  def test_create_header
+    header = @klass.new("username", "password")
+    assert_equal(
+      Wsse::UsernameTokenBuilder.create_token("username", "password", "nonce", "2009-01-01T00:00:00"),
+      header.create_token("nonce", "2009-01-01T00:00:00"))
+  end
+
+  def test_create_header__default_created
+    header = @klass.new("username", "password")
+    token  = header.create_token("nonce")
+    params = Wsse::UsernameTokenParser.parse(token)
+    assert_equal(
+      %w[Username Nonce PasswordDigest Created].sort,
+      params.keys.sort)
+  end
+
+  def test_create_header__default_nonce
+    header = @klass.new("username", "password")
+    token  = header.create_token
+    params = Wsse::UsernameTokenParser.parse(token)
+    assert_equal(
+      %w[Username Nonce PasswordDigest Created].sort,
+      params.keys.sort)
   end
 end
