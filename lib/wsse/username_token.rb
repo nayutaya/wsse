0abc1dfe8aafa7c26073832d5cc834c02332bd40
Yuya Kato <yuyakato@gmail.com>
2009-10-28 18:42:39 +0900

    TODOコメントを追加

--- lib/wsse/username_token.rb
+++ lib/wsse/username_token.rb
@@ -10,5 +10,17 @@ module Wsse
     end
 
     attr_reader :username, :digest, :nonce, :created
+
+    # TODO: self.create_nonce(size) -> str
+    # TODO: self.create_password_digest(password, nonce, created) -> str
+    # TODO: self.format_token(username, digest, nonce, created) -> str
+    # TODO: self.parse(token) -> UsernameToken
+    # TODO: self.parse_time(time) -> Time
+    # TODO: self.build(username, password, nonce, created) -> UsernameToken
+    # TODO: base64encoded_digest -> str
+    # TODO: base64encoded_nonce -> str
+    # TODO: formatted_created -> str
+    # TODO: create_token_params -> Hash
+    # TODO: create_token -> Hash
   end
 end
