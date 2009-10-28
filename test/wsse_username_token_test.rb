7e28a2acde39cbe6adeb0129489d7212976435d3
Yuya Kato <yuyakato@gmail.com>
2009-10-28 18:28:29 +0900

    UsernameTokenクラスを追加

---
+++ test/wsse_username_token_test.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+require File.dirname(__FILE__) + "/test_helper"
+require "wsse/username_token"
+
+class WsseUsernameTokenTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_ok
+    assert(true)
+  end
+end
