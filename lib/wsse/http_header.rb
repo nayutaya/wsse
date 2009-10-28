2e4bb274ba5f182529e4999d517bbb9f87226214
Yuya Kato <yuyakato@gmail.com>
2009-10-28 13:34:32 +0900

    HttpHeaderクラスを追加

--- lib/wsse/http_header.rb
+++ lib/wsse/http_header.rb
@@ -1,1 +1,12 @@
 # coding: utf-8
+
+module Wsse
+  class HttpHeader
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
+
+    attr_reader :username, :password
+  end
+end
--- test/wsse_http_header_test.rb
+++ test/wsse_http_header_test.rb
@@ -1,12 +1,22 @@
 # coding: utf-8
 
 require File.dirname(__FILE__) + "/test_helper"
+require "wsse/http_header"
 
 class WsseHttpHeaderTest < Test::Unit::TestCase
   def setup
+    @klass = Wsse::HttpHeader
   end
 
-  def test_true
-    assert true
+  def test_initialize_and_accessor1
+    header = @klass.new("username", "password")
+    assert_equal("username", header.username)
+    assert_equal("password", header.password)
+  end
+
+  def test_initialize_and_accessor2
+    header = @klass.new("a", "b")
+    assert_equal("a", header.username)
+    assert_equal("b", header.password)
   end
 end
