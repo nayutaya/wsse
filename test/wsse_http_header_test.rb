c33b8baaad590d64e2955868de009cc9acdef6b0
Yuya Kato <yuyakato@gmail.com>
2009-10-28 14:02:32 +0900

    リファクタリング
    * テストを整理

--- test/wsse_http_header_test.rb
+++ test/wsse_http_header_test.rb
@@ -6,30 +6,28 @@ require "wsse/http_header"
 class WsseHttpHeaderTest < Test::Unit::TestCase
   def setup
     @klass = Wsse::HttpHeader
+    @basic = @klass.new("username", "password")
   end
 
-  def test_initialize_and_accessor1
-    header = @klass.new("username", "password")
-    assert_equal("username", header.username)
-    assert_equal("password", header.password)
+  def test_initialize_and_accessor__1
+    assert_equal("username", @basic.username)
+    assert_equal("password", @basic.password)
   end
 
-  def test_initialize_and_accessor2
+  def test_initialize_and_accessor__2
     header = @klass.new("a", "b")
     assert_equal("a", header.username)
     assert_equal("b", header.password)
   end
 
   def test_create_token
-    header = @klass.new("username", "password")
     assert_equal(
       Wsse::UsernameTokenBuilder.create_token("username", "password", "nonce", "2009-01-01T00:00:00"),
-      header.create_token("nonce", "2009-01-01T00:00:00"))
+      @basic.create_token("nonce", "2009-01-01T00:00:00"))
   end
 
   def test_create_token__default_created
-    header = @klass.new("username", "password")
-    token  = header.create_token("nonce")
+    token  = @basic.create_token("nonce")
     params = Wsse::UsernameTokenParser.parse_token(token)
     assert_equal(
       %w[Username Nonce PasswordDigest Created].sort,
@@ -37,8 +35,7 @@ class WsseHttpHeaderTest < Test::Unit::TestCase
   end
 
   def test_create_token__default_nonce
-    header = @klass.new("username", "password")
-    token  = header.create_token
+    token  = @basic.create_token
     params = Wsse::UsernameTokenParser.parse_token(token)
     assert_equal(
       %w[Username Nonce PasswordDigest Created].sort,
@@ -48,23 +45,23 @@ class WsseHttpHeaderTest < Test::Unit::TestCase
   def test_parse_token
     token = Wsse::UsernameTokenBuilder.create_token("foo", "bar", "baz", "2000-01-01T00:00:00")
 
-    header = @klass.new("username", "password")
     expected = {
       "Username"       => "foo",
       "PasswordDigest" => "qzlKm7PqSP1MPDHUJXz5yhb0ECg=",
       "Nonce"          => "YmF6",
       "Created"        => "2000-01-01T00:00:00",
     }
-    assert_equal(expected, header.parse_token(token))
+    assert_equal(expected, @basic.parse_token(token))
   end
 
-  def test_match_username?
-    header1 = @klass.new("username", "password")
-    assert_equal(true,  header1.match_username?("Username" => "username"))
-    assert_equal(false, header1.match_username?("Username" => "USERNAME"))
+  def test_match_username__1
+    assert_equal(true,  @basic.match_username?("Username" => "username"))
+    assert_equal(false, @basic.match_username?("Username" => "USERNAME"))
+  end
 
-    header2 = @klass.new("foo", "bar")
-    assert_equal(true,  header2.match_username?("Username" => "foo"))
-    assert_equal(false, header2.match_username?("Username" => nil))
+  def test_match_username__2
+    header = @klass.new("foo", "bar")
+    assert_equal(true,  header.match_username?("Username" => "foo"))
+    assert_equal(false, header.match_username?("Username" => nil))
   end
 end
