d7765d2eaa5487ef23f82176e9ad64d1eb5e2d81
Yuya Kato <yuyakato@gmail.com>
2009-10-29 13:52:43 +0900

    リファクタリング

--- lib/wsse/authenticator.rb
+++ lib/wsse/authenticator.rb
@@ -5,16 +5,20 @@ require "wsse/username_token"
 module Wsse
   module Authenticator
     def self.authenticate(token, username, password)
-      if username != token.username
-        return :wrong_username
-      end
-
-      digest = UsernameToken.create_password_digest(password, token.nonce, token.created)
-      if digest != token.digest
-        return :wrong_password
-      end
-
+      return :wrong_username unless judge_username(token, username)
+      return :wrong_password unless judge_password(token, password)
       return :success
     end
+
+    def self.judge_username(token, username)
+      return (token.username == username)
+    end
+    private_class_method :judge_username
+
+    def self.judge_password(token, password)
+      digest = UsernameToken.create_password_digest(password, token.nonce, token.created)
+      return (token.digest == digest)
+    end
+    private_class_method :judge_password
   end
 end
